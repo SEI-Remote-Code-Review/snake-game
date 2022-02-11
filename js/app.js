48f1679dd49df5cff1bf8a2a6db12bdf3a110c41
Illia Brytun <elbrytun@gmail.com>
2022-02-11 15:00:44 -0500

    add render function

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
+const snake = [{x:5, y:5}, {x:5, y:6}, {x:5, y:7}];
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let boardCells;
+
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -18,14 +18,29 @@ const board = document.querySelector('#board');
 
 /*-------------------------------- Functions --------------------------------*/
 
+let boardCells = boardGenerator(15, 15);
+
 init();
 
-console.log(boardCells);
-boardCells[65].style.backgroundColor = 'blue';
 
 function init() {
-  boardCells = boardGenerator(15, 15);
+  render();
 }
+
+
+function render() {
+  boardCells.forEach(cell => {
+    while (cell.classList.contains('snake-body')) {
+    cell.classList.remove('snake-body');
+    }
+  })
+
+  snake.forEach(part => {
+    boardCells[part.x * 15 + part.y].classList.add('snake-body');
+  })
+}
+
+
 
 function boardGenerator(columns, rows) {
   board.style.gridTemplateRows = `repeat(${rows},5vmin)`;
@@ -37,3 +52,4 @@ function boardGenerator(columns, rows) {
   }
   return document.querySelectorAll('.board-cell');
 }
+
