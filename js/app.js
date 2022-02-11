7cc24a0f903cf0a5c702b05756c7042512a6e169
Illia Brytun <elbrytun@gmail.com>
2022-02-11 13:05:22 -0500

    add function that generates game board

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
 
 
+
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let boardCells;
 
 /*------------------------ Cached Element References ------------------------*/
 
-
-
+const board = document.querySelector('#board');
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -17,3 +17,23 @@
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+init();
+
+console.log(boardCells);
+boardCells[65].style.backgroundColor = 'blue';
+
+function init() {
+  boardCells = boardGenerator(15, 15);
+}
+
+function boardGenerator(columns, rows) {
+  board.style.gridTemplateRows = `repeat(${rows},5vmin)`;
+  board.style.gridTemplateColumns = `repeat(${columns},5vmin)`;
+  for (let i = 0; i < (columns*rows); i++) {
+    let cell = document.createElement('div');
+    cell.setAttribute('class', 'board-cell');
+    board.appendChild(cell);
+  }
+  return document.querySelectorAll('.board-cell');
+}
